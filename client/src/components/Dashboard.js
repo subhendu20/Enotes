efe74a29d772b0f400965eebe40d78e21af66005
Subhendu Sarkar <64896535+subhendu20@users.noreply.github.com>
2023-07-28 20:03:28 +0530

    Update Dashboard.js

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -6,9 +6,10 @@ import './css/Dashboard.css'
 function Dashboard() {
           const [api, setapi] = useState({ articles: [], loading: true })
           const[status,setstatus]=useState({pending:0,done:0})
+          const[companions,setcompanions]=useState({namearray:[]})
           const getapi =()=> {
 
-                    axios.get('http://localhost:3001/user/notes/fetchnote', {
+                    axios.get('/user/notes/fetchnote', {
                         withCredentials: true
                     }).then(async (res) => {
                         if (res.status === 200) {
@@ -29,7 +30,7 @@ function Dashboard() {
 
                 const getstatus =()=> {
 
-                    axios.get('http://localhost:3001/user/notes/fetchstatus', {
+                    axios.get('/user/notes/fetchstatus', {
                         withCredentials: true
                     }).then(async (res) => {
                         if (res.status === 200) {
@@ -46,9 +47,28 @@ function Dashboard() {
             
                     })
                 }
+                const getcompanions =()=> {
+
+                    axios.get('/user/notes/fetchcompanion', {
+                        withCredentials: true
+                    }).then(async (res) => {
+                        if (res.status === 200) {
+                              console.log(res.data)
+                            setcompanions({ namearray:res.data})
+            
+                        }
+                       
+                    }).catch((e) => {
+                        console.log(e)
+            
+                    })
+                }
+
                 useEffect(() => {
                     getapi()
                     getstatus()
+                    getcompanions()
+                    
             
             
                 }, [])
@@ -57,6 +77,9 @@ function Dashboard() {
     <div className="dashboard">
 
           <div className="to-do">
+            <span className="title">
+                To-do list
+            </span>
           {
                     !api.loading && <div className="note-items-dashboard">
 
@@ -95,6 +118,14 @@ function Dashboard() {
 
                     </div>
                     <div className="categories">
+                        <span className="title">Companions</span>
+                        {
+                            companions.namearray.map((e)=>{
+                                return <span className="companion-list">
+                                    <p>{e.name}</p><p>{e.occurs} Tasks</p>
+                                </span>
+                            })
+                        }
 
                     </div>
           </div>
